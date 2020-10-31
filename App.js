0a502633b9c870d0320333837e0c206ab17e013f
Hariharan Achary <hariachary7@gmail.com>
2020-11-01 00:28:16 +0530

    initial pags done

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import RootStackScreen from './screens/RootStackScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <RootStackScreen>
+
+        </RootStackScreen>
+      </NavigationContainer>
+    </>
   );
 }
 
